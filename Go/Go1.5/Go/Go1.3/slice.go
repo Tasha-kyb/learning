bc6c078efc49a2904b0053d76abbb3529958badc
Tasha-kyb <tasha-kyb@yandex.ru>
2025-09-04 11:52:12 +0300

    Delete Go/Go1.5/Go directory

--- Go/Go1.5/Go/Go1.3/slice.go
+++
@@ -1,9 +0,0 @@
-package main
-import "fmt"
-
-func main () {
-	slice := []int {1,2,3,4,5}
-	for i := len(slice)-1; i>=0; i -- {
-		fmt.Print (slice[i])
-	}
-}
--- Go/Go1.5/Go/go.mod
+++
@@ -1,5 +0,0 @@
-module Go1.4
-
-go 1.24.4
-
-require github.com/gorilla/mux v1.8.1
