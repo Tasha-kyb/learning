8cb14ad803144f4c01eb3daba895506b7b6790e9
Tasha-kyb <tasha-kyb@yandex.ru>
2025-10-03 17:26:56 +0300

    Delete RestApi directory

--- RestApi/go.mod
+++
@@ -1,7 +0,0 @@
-module RestApi
-
-go 1.24.4
-
-require github.com/google/uuid v1.6.0
-
-require github.com/gorilla/mux v1.8.1
--- RestApi/internal/domain/list.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-import "time"
-
-type List struct {
-	ID			string		`json:"id"`
-	Title 		string		`json:"title"`
-	CreatedAt	time.Time	`json:"created_at"`
-}
